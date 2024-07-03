349d0a6bd3ba246240d2be301c8a168258f99872
Harshitsengar1202 <88681128+Harshitsengar1202@users.noreply.github.com>
2024-07-03 18:18:40 +0530

    Update number_guessing.py
    We haven't yet implemented the fixed number of guess limit.

--- number_guessing.py
+++ number_guessing.py
@@ -1,13 +1,37 @@
 import math
+import random
+import time
 
-def search(array,guess):
-    low=array[0]
-    high=array[len(array)-1]
+def search(array,low,high,num):
+    guess=int(input("Enter your guess:"))
+    low=low
+    high=high
     mid=math.floor((low+high)/2)
-    if mid<guess:
-        low=mid
-        print("The new range to guess is between:",low,"&",high)
-    return mid
+    if num<guess:
+        if num<mid:
+            high=mid
+            print("The number is too far!!")
+            print("The new range to guess is between:",low,"&",high)
+            search(array,low,high,num)
+        else:
+            low=mid
+            print("The number is too low!!")
+            print("The new range to guess is between:",low,"&",high)
+            search(array,low,high,num)
+    elif num>guess:
+        if num>mid:
+            low=mid
+            print("The number is too low!!")
+            print("The new range to guess is between:",low,"&",high)
+            search(array,low,high,num)
+        else:
+            high=mid
+            print("The number is too far!!")
+            print("The new range to guess is between:",low,"&",high)
+            search(array,low,high,num)
+    
+    else:
+        print("Congratulations you guessed it right!!!!!")
 
 
 
@@ -16,7 +40,11 @@ ur=int(input("Enter upper range:"))
 temp=[]
 for i in range(lr,ur+1):
     temp.append(i)
-guess=int(input("ENter your guess:"))
-out=search(temp,guess)
-print(out)
-#search(temp,guess)
+print("The system is choosing a random number between the range.......")
+time.sleep(3)
+print("The system has choosen it's number...")
+num=random.randint(lr,ur)
+#print(num)
+low=temp[0]
+high=temp[len(temp)-1]
+search(temp,low,high,num)
