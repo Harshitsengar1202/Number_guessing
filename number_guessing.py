ff0182c4705bf84f60a40e07ba53cda35f527c51
Harshitsengar1202 <88681128+Harshitsengar1202@users.noreply.github.com>
2024-07-03 15:58:24 +0530

    Update number_guessing.py

--- number_guessing.py
+++ number_guessing.py
@@ -1,4 +1,14 @@
-#def search(array):
+import math
+
+def search(array,guess):
+    low=array[0]
+    high=array[len(array)-1]
+    mid=math.floor((low+high)/2)
+    if mid<guess:
+        low=mid
+        print("The new range to guess is between:",low,"&",high)
+    return mid
+
 
 
 lr=int(input("Enter the lower range:"))
@@ -6,5 +16,7 @@ ur=int(input("Enter upper range:"))
 temp=[]
 for i in range(lr,ur+1):
     temp.append(i)
-
-#print(temp)
+guess=int(input("ENter your guess:"))
+out=search(temp,guess)
+print(out)
+#search(temp,guess)
