36d11fef6de033cee9121f3b015f774dbff48abc
Harshitsengar1202 <88681128+Harshitsengar1202@users.noreply.github.com>
2024-07-03 15:32:47 +0530

    Update number_guessing.py

--- number_guessing.py
+++ number_guessing.py
@@ -1,1 +1,10 @@
-print("Hello")
+#def search(array):
+
+
+lr=int(input("Enter the lower range:"))
+ur=int(input("Enter upper range:"))
+temp=[]
+for i in range(lr,ur+1):
+    temp.append(i)
+
+#print(temp)
