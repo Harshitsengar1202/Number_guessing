a1071611c6585c935b9206b47ecb067c0707295b
Harshitsengar1202 <88681128+Harshitsengar1202@users.noreply.github.com>
2024-07-03 18:32:17 +0530

    Update number_guessing.py

--- number_guessing.py
+++ number_guessing.py
@@ -2,33 +2,39 @@ import math
 import random
 import time
 
-def search(array,low,high,num):
+def search(array,low,high,num,count):
+    print(f"You have only {count} chances left")
     guess=int(input("Enter your guess:"))
     low=low
     high=high
     mid=math.floor((low+high)/2)
+    if count==1:
+        print("You exhausted all your chances....")
+        print("The number was:",num)
+        return None
+    count-=1
     if num<guess:
         if num<mid:
             high=mid
             print("The number is too far!!")
             print("The new range to guess is between:",low,"&",high)
-            search(array,low,high,num)
+            search(array,low,high,num,count)
         else:
             low=mid
             print("The number is too low!!")
             print("The new range to guess is between:",low,"&",high)
-            search(array,low,high,num)
+            search(array,low,high,num,count)
     elif num>guess:
         if num>mid:
             low=mid
             print("The number is too low!!")
             print("The new range to guess is between:",low,"&",high)
-            search(array,low,high,num)
+            search(array,low,high,num,count)
         else:
             high=mid
             print("The number is too far!!")
             print("The new range to guess is between:",low,"&",high)
-            search(array,low,high,num)
+            search(array,low,high,num,count)
     
     else:
         print("Congratulations you guessed it right!!!!!")
@@ -47,4 +53,5 @@ num=random.randint(lr,ur)
 #print(num)
 low=temp[0]
 high=temp[len(temp)-1]
-search(temp,low,high,num)
+count=random.randint(5,10)
+search(temp,low,high,num,count)
